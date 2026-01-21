c340ef844ea61fa70826230029be9704cbb9f1b2
Alok Debnath <alokdebnath.in@gmail.com>
2026-01-21 15:00:32 +0530

    removed unwated code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,6 @@
 import ProductionWebView from '@/components/ProductionWebView';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return <ProductionWebView />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
